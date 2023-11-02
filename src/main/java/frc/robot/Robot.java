bb15c3c86a388fd021971aa1aea208a8b9b8c739
JackTheHack17 <101522436+JackTheHack17@users.noreply.github.com>
2023-11-02 18:37:05 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 //import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -30,9 +32,13 @@ public class Robot extends LoggedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.  
-    Logger.getInstance().recordMetadata("Project name", "TankDrive w/ ADV Scope");
     m_robotContainer = new RobotContainer();  
-    //Logger.getIntance().addDataReceiver(new WPILOGWriter("/default/telemetry/exe")); 
+    
+    Logger.getInstance().recordMetadata("Project name", "ArcadeDrive w/ ADV Scope"); 
+
+    Logger.getInstance().addDataReceiver(new WPILOGWriter(".wpilog"));  
+    Logger.getInstance().addDataReceiver(new NT4Publisher()); 
+
     Logger.getInstance().start(); 
 
   }
